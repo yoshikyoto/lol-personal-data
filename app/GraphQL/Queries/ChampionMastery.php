05e266e88c5b6c20d51b3275ccd5a94847cba680
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-05-10 21:33:20 +0900

    champion mastery が取れる！

---
+++ app/GraphQL/Queries/ChampionMastery.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use SummonersKyoto\Zen\LegendZen;
+use SummonersKyoto\Kami\ChampionMastery as KamiChampiomMastery;
+use SummonersKyoto\Kami\SummonerName;
+
+class ChampionMastery
+{
+    /**
+     * Return a value for the field.
+     *
+     * @param  null  $rootValue Usually contains the result returned from the parent field. In this case, it is always `null`.
+     * @param  mixed[]  $args The arguments that were passed into the field.
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Arbitrary data that is shared between all fields of a single query.
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+     * @return mixed
+     */
+    public function resolve(
+        $rootValue,
+        array $args,
+        GraphQLContext $context,
+        ResolveInfo $resolveInfo
+    ) {
+        $summonerNameString = $args['summonerName'];
+        /**
+         * @var LegendZen $zen
+         */
+        $zen = \App::make(LegendZen::class);
+        $summoner = $zen->welcomeSummoner(new SummonerName($summonerNameString));
+        $championMasteries = $zen->welcomeChampionMasteries($summoner->getId());
+
+        return array_map(function(KamiChampiomMastery $championMastery) {
+            return [
+                'isChestGranted' => $championMastery->isChestGranted(),
+            ];
+        }, $championMasteries);
+    }
+}
