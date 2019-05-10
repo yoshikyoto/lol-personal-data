cbe79610bb6721fec75d63cf4b9aa867553cffc3
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-05-10 20:16:47 +0900

    string value

---
+++ kyoto/Kami/ChampionKey.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace SummonersKyoto\Kami;
+
+
+class ChampionKey extends StringValue
+{
+
+}
---
+++ kyoto/Kami/StringValue.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace SummonersKyoto\Kami;
+
+
+abstract class StringValue
+{
+    private $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
