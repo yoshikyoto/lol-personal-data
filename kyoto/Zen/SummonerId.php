74a23ce263234257ca96b7f3e5bb50e567c3490a
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-05-10 19:57:28 +0900

    add summoner id class

---
+++ kyoto/Zen/SummonerId.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SummonersKyoto\Zen;
+
+class SummonerId {
+    private $value;
+
+    public function __construct(string $value) {
+        $this->value = $value;
+    }
+
+    public function __toString(): string {
+        return $this->value;
+    }
+}
